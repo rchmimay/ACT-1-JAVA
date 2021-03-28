4226616d3313d5a9886f2eee9d1ccf1d0dfc8be0
A C E R <A C E R@MIMAY>
2021-03-28 21:43:16 +0800

    add app properties

--- testwebservice-1/src/main/java/web/api/sample/controller/GreetingRest.java
+++ testwebservice-1/src/main/java/web/api/sample/controller/GreetingRest.java
@@ -1,6 +1,7 @@
 package web.api.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,12 @@ public class GreetingRest {
 	@Autowired
 	GreetingService service;
 	
+	@Value("${custom.config.key}")
+	float key;
+	
 	//To test http://localhost:8080/api/greet?x=2&y=4&z=6&w=8
 	@GetMapping("/greet")
 	public String greetByPassingParam(@RequestParam int x, @RequestParam int y, @RequestParam int z, @RequestParam int w) {		
-		return service.viewGreeting((x + y + z + w)*20.42);
+		return service.viewGreeting((x + y + z + w)*key);
 	}
 }
